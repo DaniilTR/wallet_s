162283da884b5b401ebfb609eb3aaf587e124a44
Daniil <69499105+DaniilTR@users.noreply.github.com>
2025-10-31 21:18:29 +0500

    сделанно авторизация и регистрация пользователей с использованием Spring Security и JWT.

--- secure-wallet-backend/src/main/java/com/cryptowallet/security/CustomUserDetailsService.java
+++ secure-wallet-backend/src/main/java/com/cryptowallet/security/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package com.cryptowallet.security;
 
 import com.cryptowallet.repository.UserRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
